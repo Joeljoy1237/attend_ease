4566315a34f0e371c5eec79a5971b5dbdd605c79
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-11-25 21:07:15 +0530

    feat: search filter in list users

--- src/app/api/batch/[name]/route.ts
+++ src/app/api/batch/[name]/route.ts
@@ -1,20 +1,33 @@
 import Student from "@models/Student";
 import { connectToDB } from "@utils/database";
 
-export async function GET(req: Request, { params }: { params: { name: string } }) {
-    let studentData
+export async function POST(req: Request) {
+    let studentData;
     try {
         await connectToDB();
-        const data = await params;
-        if (data.name === "all") {
+
+        // Parse the request body to get params and sortOrder
+        const { branch, sortOrder } = await req.json(); // Assuming the body contains { branch, sortOrder }
+
+        // Fetch student data based on the branch (if provided)
+        if (branch === "all") {
             studentData = await Student.find({}).lean();
+        } else {
+            studentData = await Student.find({ branch }).lean();
         }
-        else {
-            studentData = await Student.find({ branch: data.name }).lean();
-        }
+
+        // Sort the student data based on the sortOrder
+        studentData.sort((a, b) => {
+            if (sortOrder === "asc") {
+                return a.name.localeCompare(b.name); // Ascending order by name
+            } else if (sortOrder === "desc") {
+                return b.name.localeCompare(a.name); // Descending order by name
+            }
+            return 0; // Default if no valid sortOrder is provided
+        });
+
         return new Response(JSON.stringify({ studentData }), { status: 200 });
-    }
-    catch (err) {
-        return new Response(JSON.stringify({ message: "Internal Serber Error" }), { status: 500 });
+    } catch (err) {
+        return new Response(JSON.stringify({ message: "Internal Server Error" }), { status: 500 });
     }
 }
--- src/widgets/Manage/components/ManageContent.tsx
+++ src/widgets/Manage/components/ManageContent.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import BatchItem from "@components/BatchItem";
 import TitleBar from "@components/TitleBar";
 import React, { useEffect, useState } from "react";
@@ -8,21 +8,59 @@ import { FiEdit } from "react-icons/fi";
 import { FaCirclePlus } from "react-icons/fa6";
 import { MdOutlineDelete } from "react-icons/md";
 
+type Student = {
+  _id: string;
+  rollNo: string;
+  name: string;
+  admnNo: string;
+  branch: string;
+};
+
 export default function ManageContent() {
-  const [studentData, setStudentData] = useState([]);
+  const [studentData, setStudentData] = useState<Student[]>([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredStudentData, setFilteredStudentData] = useState<Student[]>([]);
+  const [sortOrder, setSortOrder] = useState("asc");
 
   useEffect(() => {
-    const res = fetchStudent();
-  }, []);
+    const fetchStudent = async () => {
+      const requestBody = {
+        branch: "all", // or a specific branch like "CSE"
+        sortOrder, // or "desc" depending on how you want to sort
+      };
 
-  const fetchStudent = async () => {
-    const res = await fetch("/api/batch/all");
+      const res = await fetch("/api/batch/all", {
+        method: "POST", // Changed to POST
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody), // Include the body data
+      });
 
-    if (res.ok) {
-      const data = await res.json();
-      console.log(data.studentData);
-      setStudentData(data.studentData);
-    }
+      if (res.ok) {
+        const data = await res.json();
+        console.log(data.studentData);
+        setStudentData(data.studentData);
+        setFilteredStudentData(data.studentData);
+      }
+    };
+
+    fetchStudent();
+  }, [sortOrder]);
+
+  useEffect(() => {
+    const filtered = studentData.filter(
+      (student) =>
+        student.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        student.admnNo.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        student.branch.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        student.rollNo.toString().includes(searchQuery)
+    );
+    setFilteredStudentData(filtered);
+  }, [searchQuery]);
+
+  const handleSortChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSortOrder(e.target.value);
   };
 
   return (
@@ -43,64 +81,95 @@ export default function ManageContent() {
               <IoSearchOutline className="text-2xl text-azure-600" />
               <input
                 type="text"
+                onChange={(e) => setSearchQuery(e.target.value)}
                 className="w-full bg-azure-50 py-3 px-2 rounded-[50px] outline-none border-none placeholder:text-azure-600"
-                placeholder="Search by batch code, name ...."
+                placeholder="Search by batch code, name, roll no ..."
               />
             </div>
           </div>
           <div className="flex-[2] flex items-center justify-end gap-5">
             <div className="bg-azure-50 rounded-[10px] p-2">
-              <span className="text-azure-600">Items count: 13</span>
+              <span className="text-azure-600">
+                Items count: {filteredStudentData.length}
+              </span>
             </div>
-            {/* Dropdown for Sorting */}
             <div className="relative">
-              <select className="px-4 py-2 text-white bg-azure-600 rounded-md outline-none appearance-none cursor-pointer">
+              <select
+                className="px-4 py-2 text-white bg-azure-600 rounded-md outline-none appearance-none cursor-pointer"
+                onChange={handleSortChange}
+              >
                 <option value="asc">Sort: Ascending</option>
                 <option value="desc">Sort: Descending</option>
               </select>
             </div>
-
-            {/* Another Button */}
             <button className="px-2 outline-none border-none">
               <LuRefreshCcw className="text-azure-600 text-2xl" />
             </button>
           </div>
         </div>
         <div className="w-full h-[1px] bg-gray-300"></div>
-
-        {/* Custom Scrollable Div Table */}
         <div>
           <div className="flex bg-azure-50 p-1">
-            <div className="flex-1 px-4 py-2 text-left text-base font-medium text-azure-600">Sl No.</div>
-            <div className="flex-1 px-4 py-2 text-left text-base font-medium text-azure-600">Roll no.</div>
-            <div className="flex-1 px-4 py-2 text-left text-base font-medium text-azure-600">Name</div>
-            <div className="flex-1 px-4 py-2 text-left text-base font-medium text-azure-600">Admission No.</div>
-            <div className="flex-1 px-4 py-2 text-left text-base font-medium text-azure-600">Class</div>
-            <div className="flex-1 px-4 py-2 text-center text-base font-medium text-azure-600">Actions</div>
+            <div className="flex-1 px-4 py-2 text-left text-base font-medium text-azure-600">
+              Sl No.
+            </div>
+            <div className="flex-1 px-4 py-2 text-left text-base font-medium text-azure-600">
+              Roll no.
+            </div>
+            <div className="flex-1 px-4 py-2 text-left text-base font-medium text-azure-600">
+              Name
+            </div>
+            <div className="flex-1 px-4 py-2 text-left text-base font-medium text-azure-600">
+              Admission No.
+            </div>
+            <div className="flex-1 px-4 py-2 text-left text-base font-medium text-azure-600">
+              Class
+            </div>
+            <div className="flex-1 px-4 py-2 text-center text-base font-medium text-azure-600">
+              Actions
+            </div>
           </div>
-          <div className="overflow-y-auto" style={{ maxHeight: "42vh" }}>
-            {studentData!.map((student: any, index) => (
-              <div
-                key={index}
-                id={student._id}
-                className="flex items-center border-b border-gray-200 p-2"
-              >
-                <div className="flex-1 px-4 py-2 text-base text-gray-700">{index + 1}</div>
-                <div className="flex-1 px-4 py-2 text-base text-gray-700">{student.rollNo}</div>
-                <div className="flex-1 px-4 py-2 text-base text-gray-700">{student.name}</div>
-                <div className="flex-1 px-4 py-2 text-base text-gray-700">{student.admnNo}</div>
-                <div className="flex-1 px-4 py-2 text-base text-gray-700">{student.branch}</div>
-                <div className="flex-1 px-4 py-2 text-center flex items-center justify-center">
-                  <button className="text-blue-500 hover:text-blue-700">
-                    <FiEdit className="text-xl" />
-                  </button>
-                  <button className="text-red-500 hover:text-red-700 ml-2">
-                    <MdOutlineDelete className="text-2xl" />
-                  </button>
+          {filteredStudentData.length !== 0 ? (
+            <div className="overflow-y-auto" style={{ maxHeight: "42vh" }}>
+              {filteredStudentData.map((student, index) => (
+                <div
+                  key={index}
+                  id={student._id}
+                  className="flex items-center border-b border-gray-200 p-2"
+                >
+                  <div className="flex-1 px-4 py-2 text-base text-gray-700">
+                    {index + 1}
+                  </div>
+                  <div className="flex-1 px-4 py-2 text-base text-gray-700">
+                    {student.rollNo}
+                  </div>
+                  <div className="flex-1 px-4 py-2 text-base text-gray-700">
+                    {student.name}
+                  </div>
+                  <div className="flex-1 px-4 py-2 text-base text-gray-700">
+                    {student.admnNo}
+                  </div>
+                  <div className="flex-1 px-4 py-2 text-base text-gray-700">
+                    {student.branch}
+                  </div>
+                  <div className="flex-1 px-4 py-2 text-center text-base text-gray-700">
+                    <div className="flex gap-2 items-center justify-center">
+                      <button className="text-azure-600">
+                        <FiEdit className="text-2xl" />
+                      </button>
+                      <button className="text-red-600">
+                        <MdOutlineDelete className="text-2xl" />
+                      </button>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>
+          ) : (
+            <div className="h-[42vh] flex items-center justify-center">
+              <span className="text-azure-600">No data found</span>
+            </div>
+          )}
         </div>
       </div>
     </div>
