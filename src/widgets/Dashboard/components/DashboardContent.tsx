c92ca76744d93f99067a32261dc8ab11b2bf085d
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-12-08 12:10:06 +0530

    feat: home page skeleton

--- src/widgets/Dashboard/components/DashboardContent.tsx
+++ src/widgets/Dashboard/components/DashboardContent.tsx
@@ -16,7 +16,7 @@ type TodaysStats = {
 
 export default function DashboardContent() {
   const [attendanceHistory, setAttendanceHistory] = useState<any[]>([]); // Store fetched attendance data
-  const [loading, setLoading] = useState<boolean>(true); // Track loading state
+  const [loadingHistory, setLoadingHistory] = useState<boolean>(true); // Track loading state for attendance history
   const [totalStudent, setTotalStudent] = useState<number>(0);
   const [error, setError] = useState<string | null>(null); // Error state
   const [todayPresent, setTodayPresent] = useState<number>(0); // Today's present count
@@ -26,13 +26,17 @@ export default function DashboardContent() {
   const [todaysStats, setTodaysStats] = useState<TodaysStats | undefined>(
     undefined
   );
+  const [loadingStats, setLoadingStats] = useState<boolean>(true); // Track loading state for today's stats
+
   const router = useRouter();
+
   const fetchAttendanceHistory = async () => {
+    setLoadingHistory(true);
     try {
       const response = await fetch("/api/attendance/getAllAttendanceCounts", {
         method: "POST",
         body: JSON.stringify({ limit: 16 }),
-      }); // Make the API request
+      });
       const data = await response.json();
 
       if (response.ok) {
@@ -59,7 +63,6 @@ export default function DashboardContent() {
         if (yesterdayAttendance) {
           setYesterdayPresent(yesterdayAttendance.presentCount);
 
-          // Calculate percentage change for present
           const presentDiff = todayAttendance
             ? ((todayAttendance.presentCount -
                 yesterdayAttendance.presentCount) /
@@ -67,14 +70,12 @@ export default function DashboardContent() {
               100
             : 0;
 
-          // Calculate absent counts
           const yesterdayAbsent =
             totalStudent - yesterdayAttendance.presentCount;
           const todayAbsent = todayAttendance
             ? totalStudent - todayAttendance.presentCount
             : totalStudent;
 
-          // Calculate percentage change for absent
           const absentDiff = yesterdayAbsent
             ? ((todayAbsent - yesterdayAbsent) / yesterdayAbsent) * 100
             : 0;
@@ -88,20 +89,23 @@ export default function DashboardContent() {
     } catch (err: any) {
       setError(err.message || "An unexpected error occurred");
     } finally {
-      setLoading(false); // Set loading to false once the request is complete
+      setLoadingHistory(false);
     }
   };
 
   const fetchTodaysStats = async () => {
+    setLoadingStats(true);
     try {
       const response = await fetch("/api/attendance/getTodaysStats", {
         method: "POST",
-      }); // Make the API request
+      });
       const data = await response.json();
       setTodaysStats(data);
       console.log(data);
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoadingStats(false);
     }
   };
 
@@ -114,69 +118,69 @@ export default function DashboardContent() {
     <div className="h-full rounded-[8px] flex flex-col space-y-8">
       {/* Top Present/Absent Section */}
       <div className="flex flex-row w-full space-x-4">
-        <div className="w-[300px] h-[180px] bg-white rounded-[5px] shadow-sm flex flex-col p-4 justify-between">
-          <div className="flex flex-row space-x-4 items-center">
-            <div className="">
-              <span className="text-xl text-azure-600 font-semibold">
-                Present
+        {loadingStats ? (
+          <>
+            <div className="w-[300px] h-[180px] bg-white rounded-[5px] shadow-sm flex flex-col p-4 justify-between">
+              <div className=" animate-pulse w-2/3 h-8 bg-gray-200 rounded-md"></div>
+              <div className=" animate-pulse w-2/12 h-14 bg-gray-200 rounded-md"></div>
+              <div className=" animate-pulse w-full h-5 bg-gray-200 rounded-md"></div>
+            </div>
+            <div className="w-[300px] h-[180px] bg-white rounded-[5px] shadow-sm flex flex-col p-4 justify-between">
+              <div className=" animate-pulse w-2/3 h-8 bg-gray-200 rounded-md"></div>
+              <div className=" animate-pulse w-2/12 h-14 bg-gray-200 rounded-md"></div>
+              <div className=" animate-pulse w-full h-5 bg-gray-200 rounded-md"></div>
+            </div>
+            <div className="w-[300px] h-[180px] bg-white rounded-[5px] shadow-sm flex flex-col p-4 justify-between">
+              <div className=" animate-pulse w-2/3 h-8 bg-gray-200 rounded-md"></div>
+              <div className=" animate-pulse w-2/12 h-14 bg-gray-200 rounded-md"></div>
+              <div className=" animate-pulse w-full h-5 bg-gray-200 rounded-md"></div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="w-[300px] h-[180px] bg-white rounded-[5px] shadow-sm flex flex-col p-4 justify-between">
+              <div className="flex flex-row space-x-4 items-center">
+                <span className="text-xl text-azure-600 font-semibold">
+                  Present
+                </span>
+                <div className="w-[2px] h-[2rem] bg-gray-400"></div>
+                <span className="mt-1 text-gray-500">Today</span>
+              </div>
+              <span className="text-4xl text-azure-600 font-semibold">
+                {todaysStats?.todaysPresentCount}
+              </span>
+              <span className="text-gray-500 text-sm">
+                {todaysStats?.attendanceChange}
               </span>
             </div>
-            <div className="w-[2px] h-[2rem] bg-gray-400"></div>
-            <div className="flex items-center justify-center">
-              <span className="mt-1 text-gray-500">Today</span>
-            </div>
-          </div>
-          <div className="">
-            <span className="text-4xl text-azure-600 font-semibold">
-              {todaysStats?.todaysPresentCount}
-            </span>
-          </div>
-          <div className="">
-            <span className="text-gray-500 text-sm">
-              {todaysStats?.attendanceChange}
-            </span>
-          </div>
-        </div>
-        <div className="w-[300px] h-[180px] bg-white rounded-[5px] shadow-sm flex flex-col p-4 justify-between">
-          <div className="flex flex-row space-x-4 items-center">
-            <div className="">
-              <span className="text-xl text-azure-600 font-semibold">
-                Absent
+            <div className="w-[300px] h-[180px] bg-white rounded-[5px] shadow-sm flex flex-col p-4 justify-between">
+              <div className="flex flex-row space-x-4 items-center">
+                <span className="text-xl text-azure-600 font-semibold">
+                  Absent
+                </span>
+                <div className="w-[2px] h-[2rem] bg-gray-400"></div>
+                <span className="mt-1 text-gray-500">Today</span>
+              </div>
+              <span className="text-4xl text-red-600 font-semibold">
+                {todaysStats?.todaysAbsentCount}
+              </span>
+              <span className="text-gray-500 text-sm">
+                {todaysStats?.absenteeChange}
               </span>
             </div>
-            <div className="w-[2px] h-[2rem] bg-gray-400"></div>
-            <div className="flex items-center justify-center">
-              <span className="mt-1 text-gray-500">Today</span>
-            </div>
-          </div>
-          <div className="">
-            <span className="text-4xl text-red-600 font-semibold">
-              {todaysStats?.todaysAbsentCount}
-            </span>
-          </div>
-          <div className="">
-            <span className="text-gray-500 text-sm">
-              {todaysStats?.absenteeChange}
-            </span>
-          </div>
-        </div>
-        <div className="w-[300px] h-[180px] bg-white rounded-[5px] shadow-sm flex flex-col p-4 justify-between">
-          <div className="flex flex-row space-x-4 items-center">
-            <div className="">
+            <div className="w-[300px] h-[180px] bg-white rounded-[5px] shadow-sm flex flex-col p-4 justify-between">
               <span className="text-xl text-azure-600 font-semibold">
                 Total Students
               </span>
+              <span className="text-4xl text-azure-600 font-semibold">
+                {totalStudent}
+              </span>
+              <span className="text-gray-500 text-sm">
+                Including all batches
+              </span>
             </div>
-          </div>
-          <div className="">
-            <span className="text-4xl text-azure-600 font-semibold">
-              {totalStudent}
-            </span>
-          </div>
-          <div className="">
-            <span className="text-gray-500 text-sm">Including all batches</span>
-          </div>
-        </div>
+          </>
+        )}
       </div>
 
       {/* Attendance History Section */}
@@ -184,20 +188,27 @@ export default function DashboardContent() {
         <div className="flex flex-col w-full space-y-6">
           <div className="flex flex-row items-center justify-between">
             <TitleBar title="Attendance history" />
-            <div className="">
-              <button
-                onClick={() => {
-                  router.push("/dashboard/attendance/history");
-                }}
-                className=""
-              >
-                <span className="font-semibold text-azure-600">View all</span>
-              </button>
-            </div>
+            <button
+              onClick={() => {
+                router.push("/dashboard/attendance/history");
+              }}
+              className=""
+            >
+              <span className="font-semibold text-azure-600">View all</span>
+            </button>
           </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-            {loading ? (
-              <div className="text-center text-gray-500">Loading...</div>
+            {loadingHistory ? (
+              Array.from({ length: 8 }).map((_, idx) => (
+                <div
+                  key={idx}
+                  className="attendance-item p-4 rounded-md shadow-sm w-[280px] bg-white bg-opacity-20 border-[1px] border-azure-100 h-[160px] flex flex-col justify-between"
+                >
+                   <div className=" animate-pulse w-3/4 h-6 bg-gray-200 rounded-md"></div>
+                   <div className=" animate-pulse w-3/5 h-7 bg-gray-200 rounded-md"></div>
+                   <div className=" animate-pulse w-3/6 h-5 bg-gray-200 rounded-md"></div>
+                </div>
+              ))
             ) : error ? (
               <div className="text-center text-red-500">{error}</div>
             ) : (
